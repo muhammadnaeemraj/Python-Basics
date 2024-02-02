f01af2059630dee59f2f36581ebd428c2cbbde7a
MuhammadNaeemRaj <muhammad.naeemraj@gmail.com>
2024-02-02 22:33:19 +0500

    First Lecture - Python Basics

---
+++ lecture1.py
@@ -0,0 +1,33 @@
+print("Hi, This is me Muhammad Naeem Raj.");
+
+ty = float(23.1);
+
+tp = 1
+
+print(str(tp))
+
+# Print string and error to see the running order
+print("This will be printed")
+print("This will cause an error")
+print("This will NOT be printed")
+
+
+import sys;
+print(sys.float_info)
+
+
+# Strings in Python
+name = "Muhammad Naeem Raj";
+name2= name.replace('Raj','Rana') #replace one word to other
+print(name[6]) #return value that in on index 6
+print(name[2:6]) #return values from index 2 to 6
+print(name[::2]) #return values of even index
+print(name[0:5:2]) #retrun values from 0 index to 5, after one value
+print(len(name)) #show the lenght of the name string
+print('This (' + name + ') is the good name.'); #concatination
+print(name2*5 + "\n"); #it will repicate the string 5 times  
+print(name2.upper()) #change lower case to upper case
+print(name2.find('R')); #it will find the first index number of Raj
+
+
+
